54e520bbd82de33cb54caf54558554b065f045a3
Nicholas Petty <nickpetty@hotmail.com>
2017-05-17 22:25:26 -0400

    Practice with let and var.

---
+++ TheBasics.playground/Contents.swift
@@ -0,0 +1,13 @@
+let maximumNumberOfLoginAttempts = 10
+var currentLoginAttempt = 5
+print(currentLoginAttempt)
+currentLoginAttempt = currentLoginAttempt + 1
+print(currentLoginAttempt)
+var x: Double = 0.0, y: String = "one point zero", z: Int = 2
+print(x, y, z)
+var red, green, blue: Int
+red = 100
+green = 200
+blue = 300
+print(red, green, blue)
+print("Red: \(red), z: \(z)", terminator: "\n")
