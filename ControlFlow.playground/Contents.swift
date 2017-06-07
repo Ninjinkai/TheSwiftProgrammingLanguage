5fccf793b4538c81389dce43d7309a05c0368671
Nicholas Petty <nickpetty@hotmail.com>
2017-06-07 18:09:59 -0400

    Switch statements.

--- ControlFlow.playground/Contents.swift
+++ ControlFlow.playground/Contents.swift
@@ -36,28 +36,56 @@ import Cocoa
 //    print("i: \(i), n: \(n)")
 //}
 
-let finalSquare = 25
-var board = [Int](repeating: 0, count: finalSquare + 1)
-board[03] = +08; board[06] = +11; board[10] = +05
-board[05] = -02; board[11] = -04; board[17] = -10
-var square = 0
-var diceRoll = 0
-while square < finalSquare {
-    diceRoll += 1
-    if diceRoll == 7 {diceRoll = 1}
-    square += diceRoll
-    if square < board.count {
-        square += board[square]
-    }
-}
-print(("Game over."))
+//let finalSquare = 25
+//var board = [Int](repeating: 0, count: finalSquare + 1)
+//board[03] = +08; board[06] = +11; board[10] = +05
+//board[05] = -02; board[11] = -04; board[17] = -10
+//var square = 0
+//var diceRoll = 0
+//while square < finalSquare {
+//    diceRoll += 1
+//    if diceRoll == 7 {diceRoll = 1}
+//    square += diceRoll
+//    if square < board.count {
+//        square += board[square]
+//    }
+//}
+//print(("Game over."))
+//
+//square = 0
+//diceRoll = 0
+//repeat {
+//    square += board[square]
+//    diceRoll += 1
+//    if diceRoll == 7 { diceRoll = 1}
+//    square += diceRoll
+//} while square < finalSquare
+//print("Game Over!")
 
-square = 0
-diceRoll = 0
-repeat {
-    square += board[square]
-    diceRoll += 1
-    if diceRoll == 7 { diceRoll = 1}
-    square += diceRoll
-} while square < finalSquare
-print("Game Over!")
+let someNumber = 1
+switch someNumber {
+case 0:
+    print("Zero")
+case 1:
+    print("One")
+default:
+    print("Some number")
+}
+
+let isThisZ = "z"
+switch isThisZ {
+case "Z", "z":
+    print("That is z")
+default:
+    print("That's not z")
+}
+
+let anotherNumber = 0
+switch anotherNumber {
+case let x where x % 2 == 0:
+    print("\(x) is even")
+case let x where x % 2 != 0:
+    print("\(x) is odd")
+default:
+    print("I don't know what this is")
+}
